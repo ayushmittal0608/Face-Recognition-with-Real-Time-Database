04f320ea53f8c62b30a07b734ef22d62751c4a5a
ayushmittal0608 <118818467+ayushmittal0608@users.noreply.github.com>
2023-06-27 10:22:37 +0530

    Add files via upload

---
+++ AddDataToDatabase.py
@@ -0,0 +1,46 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://faceattendanceproject-ed30a-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('Students')
+
+data = {
+    "Meta":
+        {
+            "name": "Mark Zuckerberg",
+            "major": "Computer",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "Microsoft":
+        {
+            "name": "Bill Gates",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "Tesla & SpaceX":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
